5d3ba0d91779efd384cb3303ef49cca56fe715ba
wgPassos <passoswilliangabriel@gmail.com>
2021-10-17 16:06:50 -0300

    to-do-list

--- 16-todo-list/script.js
+++ 16-todo-list/script.js
@@ -4,12 +4,17 @@ let main = document.querySelector("#main");
 let input =  document.querySelector("#input");
 let buttonLimpar = document.querySelector("#remove");
 let listaDeTarefas = [];
-
+let ul = document.getElementById("ul");
 
 onload = function () {
     let nome = localStorage.getItem("lista_tarefas");
-    console.log(JSON.parse(nome[0]))
-    ul.innerHTML += JSON.parse(nome);
+    // console.log(JSON.parse(nome[0]));
+    console.log(nome );
+    if (nome === null) {}
+    else {
+        listaDeTarefas.innerHTML = JSON.parse(nome);
+        criarLi();
+    }
 }
 
 
@@ -32,9 +37,9 @@ function buttonCreateList() {
             concluido: false,
         })
 
-        localStorage.setItem("lista_tarefas", JSON.stringify(listaDeTarefas));
-
+        
         criarLi(listaDeTarefas)
+        
         // console.log(input.value);
         input.value = ""
         // console.log(listaDeTarefas)
@@ -42,17 +47,17 @@ function buttonCreateList() {
 }
 
 
-let ul = document.getElementById("ul");
+
 
 function criarLi(listaDeTarefas) {
-    console.log(listaDeTarefas[0].conteudo);
+    // console.log(listaDeTarefas[0].conteudo)
     ul.innerHTML = "";
     for (var i = 0; i < listaDeTarefas.length; i++) {
         ul.innerHTML += `<li class="lessons"> ${listaDeTarefas[i].conteudo}<button class="btn-ok" onclick="tarefaFeita(this)">C</i></button> <button class="btn-excluir" onclick="tarefaExclui(this)">X</button></li>`;
 
         buttonLimpar.classList.remove("removeAllList");
-    
-        console.log(listaDeTarefas);
+    localStorage.setItem("lista_tarefas", JSON.stringify(listaDeTarefas));
+        // console.log(listaDeTarefas);
     }
     
 }
@@ -61,96 +66,23 @@ function buttonRemoveList() {
     localStorage.clear()
     ul.innerHTML = "";
     listaDeTarefas = [];
+    
 }
 
 function tarefaFeita(e) {
     console.log(e.parentElement);
     let li = e.parentElement;
     li.classList.toggle("task-done");
-    localStorage.setItem("lista_tarefas")
+    // localStorage.setItem("lista_tarefas", listaDeTarefas)
+    
 }
 
 function tarefaExclui(e) {
+    console.log(e.parentElement);
     let li = e.parentElement;
     ul.removeChild(li);
-    localStorage.getItem("lista_tarefas")
-    criarLi();
+    localStorage.removeItem("lista_tarefas")
+    
 }
 
-// console.log(listaDeTarefas);
-    // container += "<li>" + input.value + "</li>"
-        // let li = document.createElement("li");
-        // li.classList.add("lessons");
-        // li.appendChild(document.createTextNode(input.value));
-        // ul.appendChild(li);
-        // main.appendChild(ul);
-        // list = ul;
-        // input.value = "";
-        // console.log(list);
-        // buttonLimpar.classList.remove("remove");
-        
-
-        // salvarStorage();
-    // listaDeTarefas.innerHTML = "";
-    //     for  (list of listaDeTarefas) {
-    //         console.log(list.conteudo);
-    //         var li = "<li>" + list.conteudo +"</li>"
-    //         input.value = ""
-        // }
-
-    // console.log(input);
-    // console.log((listaDeTarefas[0].concluido));
-    
-    // console.log(li);
-
-// createTaskDiv(li);
-// }
-
-// function createTaskDiv(li) {
-//     // console.log(this)
-//     let div = document.createElement("div");
-
-//     let btnOk = document.createElement("button");
-//     li.children.appendChild(div);
-//     btnOk.classList.add("btn-ok");
-//     btnOk.appendChild(document.createTextNode("C"));
-//     // btnOk.innerHTML = '<i data-feather="plus"></i>'
-//     div.appendChild(btnOk);
-
-//     btnOk.setAttribute("onclick", "taskDone(this)");
-
-
-//     let btnExcluir = document.createElement("button");
-//     btnExcluir.appendChild(document.createTextNode("X"));
-//     btnExcluir.classList.add("btn-excluir");
-//     div.appendChild(btnExcluir);
-
-//     btnExcluir.setAttribute("onclick", "excluiTask(this)");
-// }
-
-// function taskDone(e) {
-//     let divButtons = e.parentElement;
-//     let liDone = divButtons.parentElement;
-    // liDone.classList.toggle("task-done");
-// }
-
-// function excluiTask(e) {
-//     let parentDiv = e.parentElement;
-//     let parentLi = parentDiv.parentElement;
-//     list.removeChild(parentLi);
-//     salvarStorage();
-// }
-
-
-// buttonLimpar.addEventListener("click", buttonRemoveList);
-
-// function buttonRemoveList() {
-//     list.innerHTML = "";
-//     salvarStorage();
-// }
-
-
-// function salvarStorage () {
-//     localStorage("salvo",  JSON.stringify(list));
-// }
 
